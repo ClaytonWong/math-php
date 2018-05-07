03599c54f8ea081fc3f333484eadd5061a84e678
ClaytonWong <c_w_serious-github@yahoo.com.au>
2018-05-07 21:22:38 +1000

    Add file.
    Add math.php

---
+++ math.php
@@ -0,0 +1,8 @@
+<?php
+  $my_rand = rand(1, 45);
+  echo "Random number between 1 & 45: $my_rand <br>";
+
+  // Convert base 10 number 12 to hexadecimal
+  $x = base_convert(12, 10, 16);
+  echo "12 in hexadecimal is $x";
+?>
